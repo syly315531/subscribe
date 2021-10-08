a5d64b51b8c0bb1c28618e538a076617e4202fec
samjoeyang <samjoeyang@gmail.com>
2021-10-08 15:31:23 +0800

    update

--- encrypt.py
+++ encrypt.py
@@ -5,10 +5,30 @@ import socket
 
 
 def vaildAddress(ipAddr,port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((ipAddr,int(port)))
-    
-    return True if result == 0 else False
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((ipAddr,int(port)))
+    except Exception as e:
+        result = False
+        print(e,ipAddr,port)
+    finally:
+        return True if result == 0 else False
+
+def decode_str(s):
+    missing_padding = len(s) % 4
+    if missing_padding != 0:
+        s += b'='* (4 - missing_padding)
+    try:
+        s = s.encode('utf-8')
+        s = bytes(s)
+        # s = base64.decodestring(s)
+        s = base64.decodebytes(s)
+        # s = base64.b64decode(bytes(s),'-_')
+        # s = base64.urlsafe_b64decode(s)
+    except Exception as e:
+        print(e,s)
+      
+    return s
 
 def decode_url(v):
     v = v.replace('\n', '')
@@ -23,28 +43,28 @@ def decode_url(v):
         
         try:
             if protocol.startswith('trojan'):
-                s = s[s.find('@')+1:s.find('?')] if index>0 else s[s.find('@')+1:]
+                if s.find('?')>0:
+                    s = s[s.find('@')+1:s.find('?')]
+                elif s.find('#')>0:
+                    s = s[s.find('@')+1:s.find('#')]
+                else:
+                    s= s[s.find('@')+1:]
+
                 return s.split(':')
         
             if index > 0:
                 s = s[:index]
-                
-                missing_padding = len(s) % 4
-                if missing_padding != 0:
-                    s += b'='* (4 - missing_padding)
-                    
-                s = base64.b64decode(s,'-_')
-                # s = base64.urlsafe_b64decode(s)
+                s = decode_str(s)
                 s = s[s.find('@')+1:]
                 return s.split(':')
                 
             else:
-                s = base64.decodestring(s)
+                s = decode_str(s)
                 s = json.loads(s)
                 return s['add'],s['port']
             
         except Exception as e:
-            print(e, index,s)
+            # print(e, index,s,v)
             return None,None
     else:
         return None,None
@@ -62,13 +82,15 @@ def encrypt_base64(filename='fly'):
         if i is not None:
             r = vaildAddress(i,p)
             if r is False:
-                print(v)
                 continue
         with open("{}.txt".format(filename),'a+') as f:
             f.writelines(v)
     
-    with open("{}.txt".format(filename),"r") as f:
-        encodeStr = base64.encodestring(f.read())
+    with open("{}.txt".format(filename),"r+") as f:
+        encodeStr = f.read()
+        encodeStr = bytes(encodeStr,'utf-8')
+        encodeStr = base64.b64encode(encodeStr)
+        encodeStr = str(encodeStr, 'utf-8')
     
     with open(filename,"w") as f:
         f.write(encodeStr)
